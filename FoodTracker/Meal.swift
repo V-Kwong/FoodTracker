22a1945255be6bb28b9f1e17de2006ec01f98dc6
V-Kwong <vincent_kwong578@hotmail.com>
2017-12-14 05:27:37 -0800

    added meal class data model and unit tests

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,33 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Vincent Kwong on 2017-12-14.
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initializer
+    init?(name: String, photo: UIImage?, rating: Int) {
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+    
+}
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,7 +56,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
 
     // MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
+        mealNameLabel.text = "Colin Yeung"
     }
     
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
